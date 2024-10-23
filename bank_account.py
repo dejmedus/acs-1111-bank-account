4d57352a4af3863bf17077e44a6f0957069cb098
Julia <email96123@gmail.com>
2024-10-23 17:20:18 -0600

    add files & initial comments

---
+++ bank_account.py
@@ -0,0 +1,37 @@
+
+class BankAccount:
+    """
+   A class to represent a bank account.
+
+   ...
+
+   Attributes
+   ----------
+   full_name : str
+      full name of the bank account owner
+   account_number : int
+       8-digit bank account number
+   balance : float
+       current balance
+
+   Methods
+   -------
+    deposit(amount=0):
+        add amount to balance and display new balance
+
+    withdraw(amount=0):
+        subtract amount from the balance and display new balance
+
+    get_balance(amount=0):
+        display current balance
+
+        Returns
+        -------
+        balance : float
+
+    add_interest(interest=0.00083):
+        add interest to the balance
+
+    print_statement():
+        display account name, account number, and balance
+   """
---
+++ main.py
@@ -0,0 +1,9 @@
+from bank_account import BankAccount
+
+# Create a new bank account instance: user: "Mitchell", account number: 03141592.
+# Deposit $400,000 into "Mitchell's" account.
+# Print a statement
+# Add interest to the account
+# Print a statement
+# Make a withdrawl of $150 (Mitchell needs some Yeezy's)
+# Print a statement
