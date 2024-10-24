336582e9488623950026a22f65be9a9d5dfab895
Julia <email96123@gmail.com>
2024-10-23 19:27:51 -0600

    add instance of bank

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from bank import Bank
 from bank_account import BankAccount
 
+# bank new member
+bank = Bank("Super Credit Union")
+bank.add_member("Bob", "77abf435")
+bank.deposit("77abf435", 1200)
+bank.statement("77abf435")
+bank.add_interest()
+bank.statement("77abf435")
+bank.withdraw("77abf435", 400)
+bank.statement("77abf435")
+
+
 # Create a new bank account instance
-mitchell_account = BankAccount("Mitchell", 31415920)
+mitchell_account = BankAccount("Mitchell", "31415920")
 
 # Deposit $400,000 into "Mitchell's" account.
 mitchell_account.deposit(400000)
