825072b90b204a1cdefec2377bc35e905c7f094b
Julia <email96123@gmail.com>
2024-10-24 15:06:13 -0600

    update methods & account type ro create account

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from bank import Bank
-from bank_account import BankAccount
+from classes.bank import Bank
 
 bank = Bank("Super Credit Union")
-bank.create_account("Bob", "77abf435")
-bank.deposit("77abf435", 1200)
-bank.statement("77abf435")
-bank.add_interest()
-bank.statement("77abf435")
-bank.withdraw("77abf435", 400)
-bank.statement("77abf435")
 
-mitchell_account = BankAccount("Mitchell", "31415920")
+bob_account = bank.create_account("Bob")
+bob_account_num = bob_account.account_number
+bank.deposit(bob_account_num, 1200)
+bank.add_interest()
+bank.statement(bob_account_num)
+bank.withdraw(bob_account_num, 400)
+bank.statement(bob_account_num)
+
+mitchell_account = bank.create_account("Mitchell", "savings", "31415920")
 mitchell_account.deposit(400000)
 mitchell_account.print_statement()
 mitchell_account.add_interest()
@@ -18,8 +18,5 @@ mitchell_account.print_statement()
 mitchell_account.withdraw(150)
 mitchell_account.print_statement()
 
-flower_account = BankAccount("Flower",)
-flower_account.deposit(40)
-flower_account.print_statement()
-flower_account.transfer(20, "31415920")
-flower_account.print_statement()
+bank.transfer("31415920", bob_account_num, 20)
+bank.statement(bob_account_num)
